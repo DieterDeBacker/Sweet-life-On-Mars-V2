4bf464020e6c9a18c2c66f8100845c60d4f19391
Dieter De Backer <dieter.debacker.y0956@student.hogent.be>
2021-06-16 23:18:33 +0200

    - Try to add 3D sound to thwomper - Added lights to flame of rocket

--- Assets/Scripts/SparksCollision.cs
+++ Assets/Scripts/SparksCollision.cs
@@ -6,7 +6,18 @@ public class SparksCollision : MonoBehaviour
 {
     [SerializeField]
     ParticleSystem sparks;
+
+    [SerializeField] AudioClip thomperSlam;
+
+    AudioSource aSource;
+
+    void Start()
+    {
+        aSource = GetComponent<AudioSource>();
+    }
     void OnCollisionEnter(Collision other) {
         sparks.Play();
+        // TODO:: Something with 3D sound
+        //aSource.PlayOneShot(thomperSlam, 0.1f);
     }
 }
