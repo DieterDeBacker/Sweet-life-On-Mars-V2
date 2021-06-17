1d3ba0a62d8bc4ba4d1d36ab5cd683895febffa7
Dieter De Backer <dieter.debacker.y0956@student.hogent.be>
2021-06-17 16:15:23 +0200

    - Adjusted UI

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,13 +4,47 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+
+    [SerializeField] AudioClip buttonClick;
+    AudioSource aSource;
+
+    void Start(){
+        aSource = GetComponent<AudioSource>();
+    }
+
+    void Update() {
+        OpenMenuInGame();
+    }
     public void PlayGame() {
         Movement.buttonsEnabled = true;
-        GetComponent<Canvas>().enabled = false;
+        GetComponent<Canvas>().enabled = false; // disabling menu screen
     }
 
     public void QuitGame(){
         Debug.Log("Quit game pressed");
         Application.Quit();
+    }
+
+    public void PlayButtonClickAudio(){
+        aSource.PlayOneShot(buttonClick);
+    }
+
+    public void OpenMenuInGame(){
+        if(Input.GetKey(KeyCode.Escape)){
+            GetComponent<Canvas>().enabled = true; // showing menu screen
+            PauseGame();
+        }
+    }
+
+    void PauseGame(){
+        Time.timeScale = 0; // pausing game
+    }
+
+    public void ResumeGame(){
+        Time.timeScale = 1; // resuming game
+    }
+
+    void DisableSpace(){
+        
     }
 }
